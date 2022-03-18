d572e0f008ae3af1d5e33aa5f37303c6a15544ec
Shamith <49478000+iamwatchdogs@users.noreply.github.com>
2022-03-19 00:29:19 +0530

    Update banner.cpp

--- banner.cpp
+++ banner.cpp
@@ -4,28 +4,24 @@
  * 
  *		Project Name 	 					: Banner
  * 		Current File Name					: Banner.cpp
- * 		Programming Language used			: C++
+ * 		Programming Language used			        : C++
  * 		Current File Type					: C++ program file ( ordinary driver program )
- * 		Project initiation Date/Time		: 19th December 2021 04:58 P.M.
- * 		Recent Data/Time of Modification	: 18th March 2022 02:35 P.M.
- * 		File Status							: Verified and working.
- * 		Author								: N.Shamith
+ * 		Project initiation Date/Time				: 19th December 2021 04:58 P.M.
+ * 		Recent Data/Time of Modification			: 19th March 2022 12:30 A.M.
+ * 		File Status						: Verified and working.
+ * 		Author							: N.Shamith
  * 
  * 		FILE DESCRIPTION :-
  * 		----------------
- * 						    This a simple driver program which is used to demonatrate the
- * 		functionalitiy / working of the "Banner" project. The main source code is written
- * 		in the header file named "banner.h" (which is also included in this source code).
- * 		The functioning ( or ) the working of this whole project is explained in detailed
- * 		in file named "README.md".
+ * 						     This a simple driver program which is used to demonatrate the functionalitiy / working of the
+ * 		"Banner" project. The main source code is written in the header file named "banner.h" (which is also included in this source code).
+ * 		The functioning ( or ) the working of this whole project is explained in detailed in file named "README.md".
  * 
  * 		DISCLAIMER :-
  * 		----------
- * 						  This project is just an imitation of "banner" command ( displays
- * 		ASCII character strings in large size on the standard output) in Linux Envirnoment. 
- * 		This was not an original idea, It's just imitaion. The soucre code is only available 
- * 		for Educational purpose and to develop it even further. No credit will be given to 
- * 		anyone, including the author of this source code.
+ * 						      This project is just an imitation of "banner" command ( displays ASCII character strings in large
+ *		size on the standard output) in Linux Envirnoment. This was not an original idea, It's just imitaion. The soucre code is only available 
+ * 		for Educational purpose and to develop it even further. No credit will be given to anyone, including the author of this source code.
  * 
  */
 
