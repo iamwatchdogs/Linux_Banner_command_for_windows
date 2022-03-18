28ba4fc83b9b2814b97ab29e970d88e1cbd1931d
Shamith <49478000+iamwatchdogs@users.noreply.github.com>
2022-03-19 00:26:30 +0530

    Update banner_cmd_arg.cpp

--- banner_cmd_arg.cpp
+++ banner_cmd_arg.cpp
@@ -4,30 +4,26 @@
  * 
  *		Project Name 	 					: Banner
  * 		Current File Name					: banner_cmd_arg.cpp
- * 		Programming Language used			: C++
+ * 		Programming Language used			        : C++
  * 		Current File Type					: C++ program file ( command line argument driver program )
- * 		Project initiation Date/Time		: 19th December 2021 05:08 P.M.
- * 		Recent Data/Time of Modification	: 18th March 2022 03:17 P.M.
- * 		File Status							: Verified and working.
- * 		Author								: N.Shamith
+ * 		Project initiation Date/Time		                : 19th December 2021 05:08 P.M.
+ * 		Recent Data/Time of Modification	                : 19th March 2022 12:26 A.M.
+ * 		File Status					 	: Verified and working.
+ * 		Author							: N.Shamith
  * 
  * 		FILE DESCRIPTION :-
  * 		----------------
- * 						    This is the closest imitation program written for the "banner"
- * 		command in Linux Envirnoment. Since the "banner" is a command in Linux Envirnoment
- * 		and the we have to pass arguments to make use of it. The concept of command line
- * 		argument plays a key role in imitating the original command as close as possible &
- * 		that's what done here. This Source code does the same as the "banner.cpp" does, but
- * 		it's done throght the concept of command line argument. The functioning ( or ) the
- * 		working of this whole project is explained in detailed in file named "README.md".
+ * 				  	This is the closest imitation program written for the "banner" command in Linux Envirnoment. Since the "banner"
+ * 		is a command in Linux Envirnoment and the we have to pass arguments to make use of it. The concept of command line argument plays a key
+ * 		role in imitating the original command as close as possible & that's what done here. This Source code does the same as the "banner.cpp"
+ * 		does, but it's done throght the concept of command line argument. The functioning ( or ) the working of this whole project is explained
+ * 		in detailed in file named "README.md".
  * 
  * 		DISCLAIMER :-
  * 		----------
- * 						  This project is just an imitation of "banner" command ( displays
- * 		ASCII character strings in large size on the standard output) in Linux Envirnoment. 
- * 		This was not an original idea, It's just imitaion. The soucre code is only available 
- * 		for Educational purpose and to develop it even further. No credit will be given to 
- * 		anyone, including the author of this source code.
+ * 				  This project is just an imitation of "banner" command ( displays ASCII character strings in large size on the
+ * 		standard output) in Linux Envirnoment. This was not an original idea, It's just imitaion. The soucre code is only available for
+ * 		Educational purpose and to develop it even further. No credit will be given to anyone, including the author of this source code.
  * 
  */
  
